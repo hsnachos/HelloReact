b1cd90f4eb60e8d751ad264a61e77b6b2a07c10c
hsnachos <hsnachos@gmail.com>
2023-05-04 10:49:29 +0900

    state 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return <MyComponent name="가나다라">리액트 자식요소 테스트</MyComponent>;
+  // return <MyComponent name="가나다라">리액트 자식요소 테스트</MyComponent>;
+  return <Counter />;
 };
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     number : 0,
+  //     fixedNumber : 0,
+  //   };
+  // }
+
+  state = {number : 0, fixedNumber : 0};
+  render() {
+    const { number, fixedNumber } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h1>{fixedNumber}</h1>
+        <button
+          onClick = {() => {
+            // this.setState({number: number + 1})
+            // this.setState({number: number + 1})
+
+            // this.setState((prevState)=>{
+            //   return { number: prevState.number + 1 };
+            // })
+            // this.setState((prevState)=>{
+            //   return { number: prevState.number + 1 };
+            // })
+
+            this.setState({number : number + 1}, ()=>{
+              console.log("방금 setState가 호출!");
+              console.log(this.state);
+            });
+
+          }}
+        >
+          +1
+          </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
