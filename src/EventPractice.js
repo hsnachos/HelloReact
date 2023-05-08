8c8acca863d39c5b08379c7d22669cd8de6c60eb
hsnachos <hsnachos@gmail.com>
2023-05-08 10:42:01 +0900

    이벤트 함수 외부 분리

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
   state = {message : ""};
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      // alert(this.state.message);
+      message : e.target.value,
+    })
+  }
+
+  handleClick(e){
+    alert(this.state.message);
+    this.setState({
+      message : ''
+    });
+  }
+
   render() {
     return (
       <div>
@@ -10,19 +31,10 @@ class EventPractice extends Component {
           name="message"
           placeholder='아무거나 입력해보세요'
           value={this.state.message}
-          onChange={(e) => {
-
-            this.setState({
-              message: e.target.value,
-            });
-
-          }}
+          onChange={this.handleChange}
         />
         <button
-          onClick={()=>{
-            alert(this.state.message);
-            this.setState({message : ""});
-          }}>
+          onClick={this.handleClick}>
           확인
         </button>
         <h2>{this.state.message}</h2>
